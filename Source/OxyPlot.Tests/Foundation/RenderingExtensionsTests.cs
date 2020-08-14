6e3786d469a1951523a88cbcce7b5a2ac054c91c
Murdo R Ergeaux <visual.melon@gmail.com>
2020-08-14 19:47:39 +0100

    Remove Geometric Clipping Tests

--- Source/OxyPlot.Tests/Foundation/RenderingExtensionsTests.cs
+++ Source/OxyPlot.Tests/Foundation/RenderingExtensionsTests.cs
@@ -40,50 +40,6 @@ namespace OxyPlot.Tests
                 rc.DrawClippedLine(clippingRectangle, points, 1, OxyColors.Black, 1, EdgeRenderingMode.Adaptive, null, LineJoin.Miter, null, received.AddRange);
                 Assert.AreEqual(0, received.Count);
             }
-
-            /// <summary>
-            /// Given a single point inside the clipping rectangle.
-            /// </summary>
-            [Test]
-            public void SinglePointInside()
-            {
-                var points = new[] { new ScreenPoint(0.5, 0) };
-                var clippingRectangle = new OxyRect(0.3, -0.5, 0.5, 1);
-                var rc = Substitute.For<IRenderContext>();
-                var received = new List<ScreenPoint>();
-                rc.DrawClippedLine(clippingRectangle, points, 1, OxyColors.Black, 1, EdgeRenderingMode.Adaptive, null, LineJoin.Miter, null, received.AddRange);
-                Assert.AreEqual(2, received.Count);
-            }
-
-            /// <summary>
-            /// Given a single point outside the clipping rectangle.
-            /// </summary>
-            [Test]
-            public void SinglePointOutside()
-            {
-                var points = new[] { new ScreenPoint(0, 0) };
-                var clippingRectangle = new OxyRect(0.3, -0.5, 0.5, 1);
-                var rc = Substitute.For<IRenderContext>();
-                var received = new List<ScreenPoint>();
-                rc.DrawClippedLine(clippingRectangle, points, 1, OxyColors.Black, 1, EdgeRenderingMode.Adaptive, null, LineJoin.Miter, null, received.AddRange);
-                Assert.AreEqual(0, received.Count);
-            }
-
-            /// <summary>
-            /// Given a line that crosses the clipping rectangle.
-            /// </summary>
-            [Test]
-            public void CrossingLine()
-            {
-                var points = new[] { new ScreenPoint(0, 0), new ScreenPoint(100, 0) };
-                var clippingRectangle = new OxyRect(30, -50, 50, 100);
-                var rc = Substitute.For<IRenderContext>();
-                var received = new List<ScreenPoint>();
-                rc.DrawClippedLine(clippingRectangle, points, 1, OxyColors.Black, 1, EdgeRenderingMode.Adaptive, null, LineJoin.Miter, null, received.AddRange);
-                Assert.AreEqual(2, received.Count);
-                Assert.AreEqual(new ScreenPoint(30, 0), received[0]);
-                Assert.AreEqual(new ScreenPoint(80, 0), received[1]);
-            }
         }
     }
 }
